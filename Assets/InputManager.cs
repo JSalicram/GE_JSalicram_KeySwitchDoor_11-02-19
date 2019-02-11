8b2b3598bab5e303ac33c895ce3a6749892e05b3
JSalicram <JSalicram@users.github.com>
2019-02-11 19:25:28 +0000

    InputWorking
    Hero moves
    Switch turns on and off
    Switch is enabled and disabled OnTriggerEnter/Exit

---
+++ Assets/InputManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            LevelManager.instance.flipTheSwitch();
+        }
+    }
+}
---
+++ Assets/LevelManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+    public static LevelManager instance;
+
+    public SwitchController theSwitch;
+
+    public bool switchEnabled;
+    public bool switchOff;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void flipTheSwitch()
+    {
+
+        if (switchEnabled == true)
+        {
+            if (switchOff == true)
+            {
+                theSwitch.turnOn();
+                switchOff = false;
+            }
+
+            else if (switchOff == false)
+            {
+                theSwitch.turnOff();
+                switchOff = true;
+            }
+        }
+    }
+
+        public void onSwitchTriggerEnter(Collider2D other)
+    {
+        Debug.Log("enabled");
+        switchEnabled = true;
+    }
+
+    public void onSwitchTriggerExit(Collider2D other)
+    {
+        Debug.Log("disabled");
+        switchEnabled = false;
+    }
+}
